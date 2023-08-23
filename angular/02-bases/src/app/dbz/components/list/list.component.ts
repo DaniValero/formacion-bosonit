cfa797c0c8ed336fb00d5d1818279ecfe93e1334
Daniel Valero <danivalero98@gmail.com>
2023-08-23 15:37:23 +0200

    Proyecto de gifs empezado

--- angular/02-bases/src/app/dbz/components/list/list.component.ts
+++ angular/02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [
+  {
+    name: "Trunks",
+    power: 10
+  }
+  ]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter()
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return
+    this.onDelete.emit(id)
+  }
 }
